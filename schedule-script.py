c2a3223e327ad5fffb45e9b20d031e78c12690f1
Victor Caveda <caveda@gmail.com>
2020-04-06 18:31:58 +0200

    Add script execution and validations logs

--- schedule-script.py
+++ schedule-script.py
@@ -1,5 +1,6 @@
 import argparse
-import logging
+import os
+import re
 import time
 from os import path
 
@@ -13,34 +14,49 @@ MONDAY = "1"
 TUESDAY = "2"
 WEDNESDAY = "3"
 THURSDAY = "4"
-FRIDAY  = "5"
-SATURDAY  = "6"
+FRIDAY = "5"
+SATURDAY = "6"
 EVERY_DAY = SUNDAY + MONDAY + TUESDAY + WEDNESDAY + THURSDAY + FRIDAY + SATURDAY
 
 
 def run_task(task_file):
     if not validate_task(task_file):
-        log (f"Execution skipped")
+        log(f"Execution skipped")
         return
+    os.system(task_file)
 
 
 def validate_task(task_file):
     """ Validates task file """
-    valid = path.exits(task) and path.isfile(task)
+    valid = path.exits(task_file) and path.isfile(task_file)
     if not valid:
-        log(f"File {task} does not exists!!")
+        log(f"File {task_file} does not exists!!")
 
 
 def validate_weekdays(weekdays):
-    pass
+    """ Validates weekdays format """
+    result = re.match("^0?1?2?3?4?5?6?$", weekdays) is not None
+    if not result:
+        log(f"Invalid weekdays format: {weekdays}!!")
+    return result
+
+def validate_time(time):
+    """ Validates time format """
+    try:
+        time.strptime(input, '%H:%M')
+        return True
+    except ValueError:
+        log(f"Invalid time format: {time}!!")
+        return False
 
 
 def set_schedule(time, weekdays, task):
     """ Sets the schedule passed as arguments"""
-    if not validate_task(task):
+    if not validate_task(task) or \
+            not validate_weekdays(weekdays) or \
+            not validate_time(time):
         return False
-    if not validate_weekdays(weekdays):
-        return False
+
     job = schedule.every()
     if SUNDAY in weekdays:
         job = job.sunday()
@@ -55,8 +71,8 @@ def set_schedule(time, weekdays, task):
     if FRIDAY in weekdays:
         job = job.friday()
     if SATURDAY in weekdays:
-        job = job.friday()
-    job.at("10:30").do(task)
+        job = job.saturday()
+    job.at(time).do(task)
 
 
 def parse_arguments():
@@ -67,8 +83,9 @@ def parse_arguments():
     parser.add_argument("--weekdays", help="Weeks of the day to execute expressed in numbers [0-6] where: \
                         0 is Sunday, 1 is Monday, and so forth.  e.g. 024 means Sundays,\
                         Tuesdays and Thursdays. Default value is 0123456 which means everyday.",
-                        default = EVERY_DAY)
+                        default=EVERY_DAY)
     return parser.parse_args()
+
 
 def main():
     """ Main function """
